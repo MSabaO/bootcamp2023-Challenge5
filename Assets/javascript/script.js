2a9c08c5b8dfda44cd043e311714c849e8297d8f
MaikaSaba <saba.maika@gmail.com>
2024-01-22 21:47:10 -0400

    added additional hours in html  and current hour function in js

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,17 +1,29 @@
+// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+// the code isn't run until the browser has finished rendering all the elements
+// in the html.
 // Display today's day and date
-let test = "test";
-var todayDate = moment().format("dddd, MMM Do YYYY");
+var todayDate = moment().format("dddd, MMM Do YYYY, hh:mm A");
 $("#currentDay").html(todayDate);
+//Collect current time
+var currentHour = dayjs().hour();
+var today = dayjs();
 
 $(document).ready(function () {
+  // TODO: Add a listener for click events on the save button. This code should
+  // use the id in the containing time-block as a key to save the user input in
+  // local storage. HINT: What does `this` reference in the click listener
+  // function? How can DOM traversal be used to get the "hour-x" id of the
+  // time-block containing the button that was clicked? How might the id be
+  // useful when saving the description in local storage?
+
   // saveBtn click listener
   $(".saveBtn").on("click", function () {
     // Get nearby values of the description in JQuery
-    var text = $(this).siblings(".description").val();
+    var textInput = $(this).siblings(".description").val();
     var time = $(this).parent().attr("id");
 
     // Save text in local storage
-    localStorage.setItem(time, text);
+    localStorage.setItem(time, textInput);
   });
 
   function timeTracker() {
@@ -54,16 +66,7 @@ $(document).ready(function () {
   timeTracker();
 });
 
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
   //
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
